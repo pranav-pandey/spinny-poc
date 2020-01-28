761fe3e4eb8eff6c1cc5609aa24e4c74b633c817
Pranav Pandey <pranav.pandey1994@gmail.com>
2020-01-28 13:04:58 +0530

    refactored serverside calls

--- server/server.js
+++ server/server.js
@@ -14,43 +14,38 @@ const app = express();
 
 app.use('/static', express.static(path.join(__dirname, '/..', '/build/static')))
 
-app.get('*', (req, res, next) => {
-  fetch('http://localhost:5000/')
-  .then((res) => res.json())
-  .then((response) => {
-    fetch('http://localhost:5000/projects')
-    .then((_res) => _res.json())
-    .then((result) => {
-      let initialData = {
-        tasks: response,
-        projects: result
-      }
-      const context = {};      
-      const app = ReactDOMServer.renderToString(
-        <StaticRouter location={req.url} context={context}>
-          <App initialData={initialData} />
-        </StaticRouter>
-      );  
-      fs.readFile(path.resolve('./build/index.html'), 'utf8', (err, data) => {
-        if (err) {
-          return res.status(500).send('An error occurred')
-        }      
-        return res.send(
-          data.replace(
-            '<div id="root"></div>',
-            `<div id="root">${app}</div>
-            <script>
-            window.__PRELOADED_STATE__ = ${JSON.stringify(initialData).replace(
-              /</g,
-              '\\u003c'
-            )}
-            </script>`
-          )
-        )
-      })
+app.get('*', async (req, res, next) => {
+  const tasks = await fetch('http://localhost:5000/');
+  const response = await tasks.json();
 
-    })
-    
+  const projects = await fetch('http://localhost:5000/projects');
+  const result = await projects.json();
+  let initialData = {
+    tasks: response,
+    projects: result
+  }
+  const context = {};      
+  const app = ReactDOMServer.renderToString(
+    <StaticRouter location={req.url} context={context}>
+      <App initialData={initialData} />
+    </StaticRouter>
+  );  
+  fs.readFile(path.resolve('./build/index.html'), 'utf8', (err, data) => {
+    if (err) {
+      return res.status(500).send('An error occurred')
+    }      
+    return res.send(
+      data.replace(
+        '<div id="root"></div>',
+        `<div id="root">${app}</div>
+        <script>
+        window.__PRELOADED_STATE__ = ${JSON.stringify(initialData).replace(
+          /</g,
+          '\\u003c'
+        )}
+        </script>`
+      )
+    )
   })
 });
 
